9fb26003127c603a6ce3daaa25734c65ee2ef4ce
Sebastião <sebastiao.f.m.coelho@gmail.com>
2024-07-12 13:15:42 +0100

    autenticacao temp

--- project/settings.py
+++ project/settings.py
@@ -134,5 +134,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'seu.email@gmail.com'
-EMAIL_HOST_PASSWORD = 'eonzidycfgbszpkk'
+EMAIL_HOST_USER = 'sebastiao.f.m.coelho@gmail.com'
+EMAIL_HOST_PASSWORD = 'plinlncinarytpqh'
+
+# LOGIN_URL = 'autenticacao:login'
